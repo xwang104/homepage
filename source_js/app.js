cce46c7e56f31a03250c454d2348f8f1fddb552c
Xiaoxiao Wang <xiaoxiaowang@Xiaoxiaos-MacBook-Pro.local>
2016-03-07 11:35:39 -0600

    list

--- source_js/app.js
+++ source_js/app.js
@@ -1,5 +1,23 @@
 var app = angular.module('mp3',['ngRoute']);
 
 app.config(function ($routeProvider) {
-
-})
+	$routeProvider
+		.when('/list', {
+			templateUrl: './partials/list.html',
+			controller: 'listCtrl',
+			controllerAs: 'list'
+		})
+		.when('/gallery', {
+			templateUrl: './partials/gallery.html',
+			controller: 'galleryCtrl',
+			controllerAs: 'gallery'
+		})
+		.when('/details/:rank', {
+			templateUrl: './partials/details.html',
+			controller: 'detailsCtrl',
+			controllerAs: 'details'
+		})
+		.otherwise({
+			redirectTo: '/list'
+		});
+});
--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,1 +1,28 @@
 /* Sample Controller */
+app.controller('listCtrl', ['$scope', '$http', function($scope, $http) {
+	$scope.searchInput = '';
+	$scope.sortBys = [
+		{
+			id: 1,
+			title: 'Title',
+			key: 'title',
+		},
+		{
+			id: 2,
+			title: 'Rank',
+			key: 'rank',
+		}
+		];
+	$scope.sortBy = $scope.sortBys[0];
+    $scope.reverse = false;
+
+	$http.get('./data/imdb250.json').
+	  success(function(data, status) {
+	  	$scope.status = status;
+	  	$scope.data = data;
+	  }).
+	  error(function(data, status) {
+	  	$scope.data = data || "Request failed";
+	  	$scope.status = status;
+	  });
+}]);
