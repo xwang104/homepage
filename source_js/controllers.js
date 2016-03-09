d5943635856a6c8a0b1dbf6aea80644be03ecc74
Xiaoxiao Wang <xiaoxiaowang@Xiaoxiaos-MacBook-Pro.local>
2016-03-08 22:02:53 -0600

    add movie genres

--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,5 +1,5 @@
 /* Sample Controller */
-app.controller('listCtrl', function($scope, movieFactory) {
+app.controller('listCtrl', ['$scope', 'movieFactory', function($scope, movieFactory) {
 	$scope.searchInput = '';
 	$scope.sortBys = [
 		{
@@ -24,11 +24,11 @@ app.controller('listCtrl', function($scope, movieFactory) {
     	});
 
 
-});
+}]);
 
-app.controller('galleryCtrl', function($scope, movieFactory) {
+app.controller('galleryCtrl', ['$scope', 'movieFactory', function($scope, movieFactory) {
 
-	$scope.genres = ['Action', 'Adventure', 'Crime', 'Comedy', 'Drama', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'Western'];
+	$scope.genres = ['Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 'Drama', 'Family', 'Fantasy', 'Film-Noir', 'History', 'Horror', 'Music', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western'];
 	$scope.select = {selectGenre: ''};
 
 
@@ -38,9 +38,9 @@ app.controller('galleryCtrl', function($scope, movieFactory) {
     		$scope.data = jsonData;
     	});
 
-});
+}]);
 
-app.controller('detailsCtrl', function($scope, movieFactory, $routeParams) {
+app.controller('detailsCtrl', ['$scope', 'movieFactory', '$routeParams', function($scope, movieFactory, $routeParams) {
 	$scope.myRank = $routeParams.rank;
 	$scope.preRank = parseInt($scope.myRank) - 1;
 	$scope.nextRank = parseInt($scope.myRank) + 1;
@@ -59,7 +59,7 @@ app.controller('detailsCtrl', function($scope, movieFactory, $routeParams) {
     	});
 
 
-});
+}]);
 
 
 
