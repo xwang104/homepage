e3c5012a9c7e080e7f8921c0fafef69c6c3614bd
Xiaoxiao Wang <xwang104@illinois.edu>
2017-01-21 19:04:05 +0000

    home and courses

--- source_js/app.js
+++ source_js/app.js
@@ -2,34 +2,20 @@ var app = angular.module('mp3',['ngRoute']);
 
 app.config(function ($routeProvider) {
 	$routeProvider
-		.when('/list', {
-			templateUrl: './partials/list.html',
-			controller: 'listCtrl',
-			controllerAs: 'list'
+		.when('/home', {
+			templateUrl: './partials/home.html',
 		})
-		.when('/gallery', {
-			templateUrl: './partials/gallery.html',
-			controller: 'galleryCtrl',
-			controllerAs: 'gallery'
+		.when('/courses', {
+			templateUrl: './partials/courses.html',
 		})
-		.when('/details/:rank', {
-			templateUrl: './partials/details.html',
-			controller: 'detailsCtrl',
-			controllerAs: 'details'
+		.when('/projects', {
+			templateUrl: './partials/projects.html',
+		})
+		.when('/more', {
+			templateUrl: './partials/more.html',
 		})
 		.otherwise({
-			redirectTo: '/list'
+			redirectTo: '/home'
 		});
 });
 
-app.factory('movieFactory', function movieFactory($http) {
-	var exports = {};
-	exports.getMovies = function() {
-		return $http.get('./data/imdb250.json')
-			.error(function(data) {
-				console.log('There was an error', data);
-			});
-	};
-	return exports;
-
-});
--- source_js/script.js
+++ source_js/script.js
@@ -1,1 +1,14 @@
 // Write any custom javascript functions here
+
+//-----------------------navigation bar sticky and resize-------------------
+
+    $('#nonresponsive-topbar').on('sticky.zf.stuckto:top', function(){
+    $(this).addClass('shrink');
+    $('#title').show();
+}).on('sticky.zf.unstuckfrom:top', function(){
+    $(this).removeClass('shrink');
+    $('#title').hide();
+})
+    $('#responsive-topbar').find('a').click(function() {
+        $('#responsive-topbar').delay(500).fadeOut();
+    })
